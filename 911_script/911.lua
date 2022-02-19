c47e23d6f7c1d1a8ad7a5a22c8cbd47ed9bd3dcf
Max W <87267714+FuriousF0xGG@users.noreply.github.com>
2022-02-19 15:28:57 +0000

    updated 911.lua
    simplified the 911.lua

--- 911_script/911.lua
+++ 911_script/911.lua
@@ -1,5 +1,5 @@
-RegisterCommand("911", function(source, args, rawCommand)
+RegisterCommand("911", function(source, args, raw)
     TriggerClientEvent('mythic_notify:client:SendAlert', -1, {
          type = 'inform',
          text = "Name: " .. GetPlayerName(source) .. " called 911, for the reason: " .. table.concat (args, " ") .. ""})
-     end, false) --set to true to use ace perms (this would restrict the command /911)
+     end)
